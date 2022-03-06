21d1c2264bffeaab6e53c43eb4f683fe4ede67e1
Marcos Cordeiro <marcosc974@gmail.com>
2022-03-06 13:30:51 -0300

    Carousel principal organizado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function startTimer(duration, display,display2, display3) {
-    var timer = duration, hours,minutes, seconds;
+    var timer = duration, hours, minutes, seconds;
     setInterval(function () {
-        hours = parseInt(timer /60,10)
+        hours = parseInt(timer /60)
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
         hours = hours < 10 ? "0" + hours : hours;
@@ -17,9 +17,15 @@ function startTimer(duration, display,display2, display3) {
     }, 1000);
 }
 window.onload = function () {
-    var duration = 60 * 5; // Converter para segundos
+    var duration = 60 * 3; // Converter para segundos
         display = document.querySelector('#horas'); // selecionando o timer
         display2 = document.querySelector('#minutos');
         display3 = document.querySelector('#segundos');
     startTimer(duration, display, display2, display3); // iniciando o timer
 };
+// Configurando o Splid Slider
+var splide = new Splide('.splide');
+splide.mount();
+
+var splide2 = new Splide('#splide2');
+splide2.mount();
